4c73ae06579f1a6b744e14eaf26fee208caee55b
SUNIL K S <sunilks85@gmail.com>
2026-05-22 13:49:04 +0530

    Update conftest.py

--- SeleniumPython/conftest.py
+++ SeleniumPython/conftest.py
@@ -14,7 +14,12 @@ def pytest_addoption(parser):
 def test_browser_instance(request):
     browser_name = request.config.getoption("--browser_name")
     if browser_name == "chrome":
-        driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage") 
+        driver = webdriver.Chrome(options=options)
+        #driver = webdriver.Chrome()
     elif browser_name == 'edge':
         driver = webdriver.Edge()
     elif browser_name == "safari":
@@ -22,11 +27,7 @@ def test_browser_instance(request):
     elif browser_name == "Firefox":
         driver = webdriver.Firefox()
 
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage") 
-    driver = webdriver.Chrome(options=options)
+
     
     driver.maximize_window()
     #ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('s').key_up(Keys.SHIFT).key_up(Keys.CONTROL).perform()
