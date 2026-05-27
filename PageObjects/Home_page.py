547e46179d85013f1ea9faeb7371784ce1e94405
SUNIL K S <sunilks85@gmail.com>
2026-05-27 12:02:52 +0530

    Update Home_page.py

--- PageObjects/Home_page.py
+++ PageObjects/Home_page.py
@@ -14,7 +14,7 @@ class HomePage:
         wait = WebDriverWait(self.driver, 20)
         #element = wait.until(Expected_Conditions.visibility_of_element_located(self.homepage_button))
         #self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
-        #element = self.driver.find_element(*self.homepage_button )
+        element = self.driver.find_element(*self.homepage_button )
         ActionChains(self.driver).move_to_element(element).pause(2).perform()
 
         #adding submenu() code here for testing
